ec425f884430f901159d7be833133a42e95580b4
folorunsho jacob <folorunshojacob05@gmail.com>
2024-08-06 11:36:56 +0100

    Other required Classes

---
+++ ContentClass.kt
@@ -0,0 +1,16 @@
+package com.example.homify
+
+// Property Class
+class ContentClass (
+    var pid: Int,
+    var uid: Int,
+    var cnt_type: String,
+    var cnt_form: String,
+    var cnt_town: String,
+    var cnt_code: String,
+    var cnt_address: String,
+    var cnt_bed: Int,
+    var cnt_bath: Int,
+    var cnt_rent: String,
+    var files: List<String>
+)
---
+++ FeedbackCLass.kt
@@ -0,0 +1,9 @@
+package com.example.homify
+
+class FeedbackCLass(
+    var cid: Int,
+    var uname: String,
+    var telephone: String,
+    var option: String,
+    var message: String
+)
