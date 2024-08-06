d0157c7313e7470d2edd0078a0bef519aa62fbd1
folorunsho jacob <folorunshojacob05@gmail.com>
2024-08-06 11:34:02 +0100

    System Classes

---
+++ GlideClass.kt
@@ -0,0 +1,8 @@
+package com.example.homify
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+@GlideModule
+class GlideClass : AppGlideModule()
